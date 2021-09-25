2701a179ef1865e4a619e6c45c1434efae4493cb
Tunji Mafe <59024298+morebymafe@users.noreply.github.com>
2021-09-25 17:55:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const countdown = () => {
+    const countDate = new Date("September 26, 2021 00:00:00").getTime();
+    const now = new Date().getTime();
+    const timeLeft = countDate - now;
+
+
+    const sec = 1000;
+    const min = sec * 60;
+    const hr = min * 60;
+    
+    // Countdown calculations
+    const hour = Math.floor(timeLeft / hr);
+    const minute = Math.floor((timeLeft % hr) / min );
+    const second = Math.floor((timeLeft % min) / sec);
+
+    // display 
+    document.querySelector('.hour').innerHTML = hour + ":";
+    document.querySelector('.min').textContent = minute + ":";
+    document.querySelector('.sec').textContent = second;
+}
+
+setInterval(countdown, 1000);
